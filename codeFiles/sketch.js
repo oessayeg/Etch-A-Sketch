f98bb5bc464ebda3da60524b98c5821b4bc74241
Othmane <offissayegh001@gmail.com>
2023-05-13 14:47:14 +0100

    Add function to create the grid with custom size

--- codeFiles/sketch.js
+++ codeFiles/sketch.js
@@ -1,3 +1,27 @@
+function makeGrid(gridSize)
+{
+	const mainEtchDiv = document.querySelector("#etch");
+
+	for (let i = 0; i < gridSize; i++)
+	{
+		const row = document.createElement("div");
+
+		row.classList.add("row");
+		for (let j = 0; j < gridSize; j++)
+		{
+			const square = document.createElement("div");
+
+			square.classList.add("square");
+			row.appendChild(square);
+			square.style.width = `${420 / gridSize - 2}`;
+			square.style.height = `${420 / gridSize - 2}`;
+		}
+		mainEtchDiv.appendChild(row);
+	}
+}
+
+makeGrid(50);
+
 // Color Picker
 // Grid size
 // Reset
